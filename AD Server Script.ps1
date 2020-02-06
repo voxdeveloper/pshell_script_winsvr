46393b2cb0cf2f4ad2a167a98c7cd5f145402dd1
Rivo.H.W <voxiox@users.noreply.github.com>
2020-02-06 09:01:59 +0800

    Initial commit

---
+++ AD Server Script.ps1
@@ -0,0 +1,19 @@
+﻿#To display the expiration date rather than the password last set date, use this command
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" |
+Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}
+
+#To display the expiration date rather than the password last set date, use this command.
+get-aduser -filter * -properties passwordlastset, passwordneverexpires |ft userPrincipalName,Name, passwordlastset, Passwordneverexpires
+#passwordneverexpires -like "*false"' 
+get-aduser -filter  *  -properties passwordlastset, passwordneverexpires |ft userPrincipalName,Name, passwordlastset, Passwordneverexpires
+
+#search like -Filter 'passwordneverexpires -like "*false"'
+
+#search based ou
+Get-ADUser -Filter * -SearchBase "OU=fad,DC=data,DC=wpsmanado,DC=COM"   -properties passwordlastset, passwordneverexpires | Format-Table userprincipalName,passwordlastset,Passwordneverexpires | Where-Object {$_.passwordneverexpires}
+
+#search based ou w/ filter object "true"
+Get-ADUser -Filter * -SearchBase "OU=fad,DC=data,DC=wpsmanado,DC=COM"   -properties passwordlastset,passwordneverexpires | Where-Object {$_.passwordneverexpires -eq "true"} | Format-Table userprincipalName,passwordlastset,Passwordneverexpires
+
+#search based ou w/ filter object "true" + export 
+Get-ADUser -Filter * -SearchBase "OU=fad,DC=data,DC=wpsmanado,DC=COM"   -properties passwordlastset,passwordneverexpires | Where-Object {$_.passwordneverexpires -eq "true"} | Format-Table userprincipalName,passwordlastset,Passwordneverexpires | export-csv X:\powershell\export\password_expiring_test.csv -NoTypeInformation
